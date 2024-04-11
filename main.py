ad51b743964c3e618a7d94e9a67d90e002a55c1f
clrdl <clro0oln@gmail.com>
2024-04-11 19:30:09 +0900

    Describe your changes here

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    num_map = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_map:
-            return [num_map[complement], i]
-        num_map[num] = i
-    return []
+    # 원래 인덱스를 포함하여 정렬
+    indexed_nums = sorted((num, i) for i, num in enumerate(nums))
+    left, right = 0, len(nums) - 1
 
+    while left < right:
+        current_sum = indexed_nums[left][0] + indexed_nums[right][0]
+        if current_sum == target:
+            return [indexed_nums[left][1], indexed_nums[right][1]]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+    return []
